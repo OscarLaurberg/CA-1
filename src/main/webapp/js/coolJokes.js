525a16e911f9cc8d81650cbf69b2bd79c76a6b26
oscar laurberg <oscarlaurberg@hotmail.com>
2020-02-20 23:38:13 +0100

    added some small and fun features

--- src/main/webapp/js/coolJokes.js
+++ src/main/webapp/js/coolJokes.js
@@ -21,27 +21,41 @@ function getJokeFromId() {
     if (jokeId > amountOfJokes || jokeId < 1) {
         document.getElementById('tableBody').innerHTML = "Joke Id doesn't exist - try again";
 
-    }else{
-        
-    
-    (fetch("api/jokes/" + jokeId))
-            .then(res => res.json())
-            .then(data => {
-                let jokeFromId = `<tr><td>${data.id}</td><td>${data.punchLine}</td><td>${data.category}</td></tr>`;
-                document.getElementById('tableBody').innerHTML = jokeFromId;
+    } else {
 
-            })
-}}
+
+        (fetch("api/jokes/" + jokeId))
+                .then(res => res.json())
+                .then(data => {
+                    let jokeFromId = `<tr><td>${data.id}</td><td>${data.punchLine}</td><td>${data.category}</td></tr>`;
+                    document.getElementById('tableBody').innerHTML = jokeFromId;
+
+                })
+    }
+}
 ;
+
 
 function getRandomJoke() {
     fetch("api/jokes/random")
             .then(res => res.json())
             .then(data => {
-                let randomJoke = `<table><tr><td> ${data.id}</td><td>${data.punchLine}</td><td>${data.category}</td></tr>`;
+                let randomJoke = `<tr><td> ${data.id}</td><td>${data.punchLine}</td><td>${data.category}</td></tr>`;
                 document.getElementById('tableBody').innerHTML = randomJoke;
             });
 }
+
+
+function getRandomChuckNorrisJoke() {
+    fetch("https://api.chucknorris.io/jokes/random")
+            .then(res => res.json())
+            .then(data => {
+                let randomCNJoke = `<tr><td> ${data.id}</td><td>${data.value}</td><td>No category</td></tr>`;
+        document.getElementById('tableBody').innerHTML = randomCNJoke;
+    }
+            );
+}
+;
 
 const populateTable = data => {
     const dataArray = data.map(data => `<tr><td>${data.id}</td><td>${data.punchLine}</td><td>${data.category}</td></tr>`);
@@ -51,3 +65,4 @@ const populateTable = data => {
 document.getElementById('getJoke').addEventListener('click', getJokeFromId);
 document.getElementById('getAllJokes').addEventListener('click', getAllJokes);
 document.getElementById('getRandomJoke').addEventListener('click', getRandomJoke);
+document.getElementById('getRandomCNJoke').addEventListener('click', getRandomChuckNorrisJoke);
